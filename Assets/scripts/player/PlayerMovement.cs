4716397227ce4d80d2fa517a2b343a713c32d0d9
Tong <tong.pnk@gmail.com>
2021-05-09 23:22:51 +0200

    ParrelSync and Mirror

--- Assets/scripts/player/PlayerMovement.cs
+++ Assets/scripts/player/PlayerMovement.cs
@@ -1,9 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour{
 
     public float moveSpeed = 5f;
 
